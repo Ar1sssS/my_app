08a9031016a1f5620e800bb5b58586431cec06b1
Aris <kuka.qwerty01@gmail.com>
2024-09-30 08:03:44 +0600

    The main app is ready and pages index, about too

--- main/views.py
+++ main/views.py
@@ -6,4 +6,9 @@ def index(request):
     return render(request, 'main/index.html')
 
 def about(request):
-    return HttpResponse('About page')
+    return render(request, 'main/about.html')
+
+
+
+
+
